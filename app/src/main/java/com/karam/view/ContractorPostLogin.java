0ef38f586b809ab740b8b56dc8ee200d1ac78364
Ajay Nair <ajaynair59@gmail.com>
2020-06-10 17:51:30 -0400

    Added UI skeleton (#12)
    * KAR-27: Added UI skeleton

---
+++ app/src/main/java/com/karam/view/ContractorPostLogin.java
@@ -0,0 +1,89 @@
+package com.karam.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+/**
+ *  Contractor's landing page once they login
+ *  The contractor can search for laborers from this page
+ */
+public class ContractorPostLogin extends AppCompatActivity {
+
+    /**
+     * Handle what happens when the activity is created
+     * @param savedInstanceState: null for now
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contractor_post_login);
+        assignListenerToViews();
+
+        // Attach action bar to the activity
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    /**
+     * Assign all listener to different views of the activity
+     */
+    private void assignListenerToViews() {
+        SearchView laborerSearch = (SearchView) findViewById(R.id.search);
+        laborerSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ContractorPostLogin.this, ContractorPostLoginSearch.class));
+            }
+        });
+    }
+
+    // TODO: This function can be moved to a separate menu class as its
+    // used by all activity class
+    /**
+     * Set up menu options
+     * @param menu: Menu options (https://pasteboard.co/Jc4U58s.png) to be shown in the activity
+     * @return: true on no error
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        menu.removeItem(R.id.user_settings);
+        menu.removeItem(R.id.check_status);
+        return true;
+    }
+
+    // TODO: This function can be moved to a separate menu class as its
+    // used by all activity class
+    /**
+     *  Responds to menu option (https://pasteboard.co/Jc4U58s.png) of this activity
+     * @param item: The item in the menu that is selected
+     * @return: return false in case of error, true otherwise
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.logout):
+                startActivity(new Intent(ContractorPostLogin.this, LoginPage.class));
+                return true;
+            default:
+                Toast.makeText(getApplicationContext(), "Oops! Error. You shouldn't be seeing this message",
+                        Toast.LENGTH_SHORT).show();
+                // Add code to report bug
+                return false;
+        }
+    }
+
+    // TODO: The back button does not work for this page. Fix the issue.
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(getApplicationContext(), "back pressed", Toast.LENGTH_SHORT).show();
+    }
+}
---
+++ app/src/main/java/com/karam/view/WorkRequestFriend.java
@@ -0,0 +1,83 @@
+package com.karam.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+/**
+ * Page for a user to register their friend as a laborer for a job request
+ */
+public class WorkRequestFriend extends AppCompatActivity {
+
+    /**
+     * Handle what happens when the activity is created
+     * @param savedInstanceState: null for now
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.work_request_friend);
+        assignListenerToViews();
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    /**
+     * Assign all listener to different views of the activity
+     */
+    private void assignListenerToViews() {
+        Button laborerReg = (Button) findViewById(R.id.register);
+        laborerReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(WorkRequestFriend.this, LaborerStatusPage.class));
+            }
+        });
+    }
+
+    // TODO: This function can be moved to a separate menu class as its
+    // used by all activity class
+    /**
+     * Set up menu options
+     * @param menu: Menu options (https://pasteboard.co/Jc4U58s.png) to be shown in the activity
+     * @return: true on no error
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    // TODO: This function can be moved to a separate menu class as its
+    // used by all activity class
+    /**
+     *  Responds to menu option (https://pasteboard.co/Jc4U58s.png) of this activity
+     * @param item: The item in the menu that is selected
+     * @return: return false in case of error, true otherwise
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.logout):
+                startActivity(new Intent(WorkRequestFriend.this, LoginPage.class));
+                break;
+            case (R.id.user_settings):
+                Toast.makeText(WorkRequestFriend.this, "Support not added", Toast.LENGTH_SHORT).show();
+                break;
+            case (R.id.check_status):
+                startActivity(new Intent(WorkRequestFriend.this, LaborerStatusPage.class));
+                break;
+            default:
+                Toast.makeText(WorkRequestFriend.this, "Oops! Error", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+}
