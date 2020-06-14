425f3f1580446030df1c829f887eec3258cce7cb
Ajay Nair <ajaynair59@gmail.com>
2020-06-14 17:26:24 -0400

    Added support for localization

--- app/src/main/java/com/karam/view/LandingPage.java
+++ app/src/main/java/com/karam/view/LandingPage.java
@@ -1,17 +1,26 @@
 package com.karam.view;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 /**
  * Landing page of the app for a user who have not logged in
@@ -20,35 +29,40 @@ public class LandingPage extends AppCompatActivity {
     private static String[] languages = {"English",
             "Hindi",
             "Marathi"};
+    ArrayAdapter<String> adapter;
 
     /**
      * Handle what happens when the activity is created
+     *
      * @param savedInstanceState: null for now
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
-        ArrayAdapter<String> adapter = getAdapterWithLanguages();
-        ListView languageList = (ListView) findViewById(R.id.languageList);
+        getAdapterWithLanguages();
+        ListView languageList = findViewById(R.id.languageList);
         languageList.setAdapter(adapter);
         languageList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(getApplicationContext(), (String) adapterView.getItemAtPosition(i) + " not available",
-                        Toast.LENGTH_SHORT).show();
+                //Toast.makeText(getApplicationContext(), (String) adapterView.getItemAtPosition(i) + " not available",
+                //        Toast.LENGTH_SHORT).show();
+                ((LanguageArrayAdapter)adapter).setLocale(getApplicationContext(), i, LandingPage.this);
             }
         });
 
         assignListenerToViews();
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
     }
 
     // TODO: This function can be moved to a separate menu class as its
     // used by all activity class
+
     /**
      * Set up menu options
+     *
      * @param menu: Menu options (https://pasteboard.co/Jc4U58s.png) to be shown in the activity
      * @return: false so that menu option (3 dots) is not shown
      */
@@ -88,12 +102,11 @@ public class LandingPage extends AppCompatActivity {
 
     /**
      * Create an adapter for language option for the user
+     *
      * @return: adapter that contains all the languages
      */
-    private ArrayAdapter<String> getAdapterWithLanguages() {
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+    void getAdapterWithLanguages() {
+        adapter = new LanguageArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, languages);
-
-        return adapter;
     }
 }
---
+++ app/src/main/java/com/karam/view/LanguageArrayAdapter.java
@@ -0,0 +1,95 @@
+package com.karam.view;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.HorizontalScrollView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+class LanguageArrayAdapter<T> extends ArrayAdapter<String> {
+    Context context;
+
+    public LanguageArrayAdapter(Context context, int simple_list_item_1, String [] languages) {
+        super(context, simple_list_item_1, languages);
+        this.context = context;
+    }
+
+    public void setLocale(Context c, int i, Activity a) {
+        Locale myLocale;
+        switch (i) {
+            case 1:
+                myLocale = new Locale("mr");
+                break;
+            case 2:
+                myLocale = new Locale("hi");
+                break;
+            default:
+                myLocale = new Locale("en");
+                break;
+        }
+
+        Resources res = a.getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(a, a.getClass());
+        a.finish();
+        a.startActivity(refresh);
+    }
+
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        view = super.getView(position, view, parent);
+        TextView t;
+        switch (position) {
+            case 1:
+                t = (TextView) view;
+                t.setText(R.string.marathi);
+
+                break;
+            case 2:
+                t = (TextView) view;
+                t.setText(R.string.hindi);
+                break;
+            default:
+                t = (TextView) view;
+                t.setText(R.string.english);
+                break;
+        }
+/*
+        if (position == 1 || position == 2) {
+            // Typeface face = Typeface.createFromAsset(context.getAssets(), "font/devnagri.ttf");
+            // ((TextView) view).setTypeface(face);
+            // Toast.makeText(context.getApplicationContext(), ((TextView) view).getText(),
+                    //Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Typeface face = Typeface.DEFAULT;
+            ((TextView) view).setTypeface(face);
+        }
+        Toast.makeText(context.getApplicationContext(), "Called",
+                Toast.LENGTH_SHORT).show();
+        */
+        return view;
+    }
+}
