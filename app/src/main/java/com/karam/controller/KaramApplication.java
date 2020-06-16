da775196aec25b24c2131961770264fb6735b023
Ajay Nair <ajaynair59@gmail.com>
2020-06-16 00:03:23 -0400

    Added more strings in hindi and marathi

--- app/src/main/java/com/karam/controller/KaramApplication.java
+++ app/src/main/java/com/karam/controller/KaramApplication.java
@@ -11,6 +11,12 @@ import com.karam.service.NotifyWorker;
 import java.util.concurrent.TimeUnit;
 
 /**
+ * Warning: Do not store anything that you wish to persist in this class
+ * The data can be lost if the system crashes and restarts or if the
+ * Application goes to the background and comes back.
+ **/
+
+/**
  * Extends the whole application class
  */
 public class KaramApplication extends Application {
