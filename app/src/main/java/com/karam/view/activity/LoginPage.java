36e3eff332a3dab3e98c51284683aa5b77494cd4
Ajay Nair <ajaynair59@gmail.com>
2020-06-23 15:30:50 -0400

    Detects user type and jumps to respective landing page

--- app/src/main/java/com/karam/view/activity/LoginPage.java
+++ app/src/main/java/com/karam/view/activity/LoginPage.java
@@ -3,8 +3,11 @@ package com.karam.view.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -34,24 +37,51 @@ public class LoginPage extends AppCompatActivity {
         laborerReg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginPage.this, ContractorPostLogin.class));
+                EditText name = findViewById(R.id.loginUserNameText);
+                if (name.getText().toString().equals("l")) {
+                    Toast.makeText(getApplicationContext(), "laborer selected",
+                            Toast.LENGTH_SHORT).show();
+
+                    startActivity(new Intent(LoginPage.this, LaborerStatusPage.class));
+                } else {
+                    startActivity(new Intent(LoginPage.this, ContractorPostLogin.class));
+                }
             }
         });
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
     }
 
-    // TODO: This function can be moved to a separate menu class as its
-    // used by all view.activity class
-
     /**
      * Set up menu options
      *
      * @param menu: Menu options (https://pasteboard.co/Jc4U58s.png) to be shown in the view.activity
-     * @return: false so that menu option (3 dots) is not shown
+     * @return: true on no error
      */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        return false;
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_logged_out, menu);
+        return true;
+    }
+
+    /**
+     * Responds to menu option (https://pasteboard.co/Jc4U58s.png) of this view.activity
+     *
+     * @param item: The item in the menu that is selected
+     * @return: return false in case of error, true otherwise
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.about_us):
+                startActivity(new Intent(LoginPage.this, AboutUs.class));
+                return true;
+            default:
+                Toast.makeText(getApplicationContext(), "Oops! Error. You shouldn't be seeing this message",
+                        Toast.LENGTH_SHORT).show();
+                // Add code to report bug
+                return false;
+        }
     }
 }
