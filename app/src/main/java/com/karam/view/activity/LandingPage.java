2d9f9e6702eebd18d58e25a600d54591e932cb11
Ajay Nair <ajaynair59@gmail.com>
2020-06-23 15:42:36 -0400

    Modifications to add menu

--- app/src/main/java/com/karam/view/activity/LandingPage.java
+++ app/src/main/java/com/karam/view/activity/LandingPage.java
@@ -1,19 +1,14 @@
 package com.karam.view.activity;
 
 import android.content.Intent;
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,17 +16,14 @@ import androidx.appcompat.widget.Toolbar;
 
 import com.karam.view.LanguageArrayAdapter;
 
-import java.util.ArrayList;
-import java.util.Locale;
 
 /**
  * Landing page of the app for a user who have not logged in
  */
 public class LandingPage extends AppCompatActivity {
-    private static String[] languages = {"English",
-            "Hindi",
-            "Marathi"};
+
     ArrayAdapter<String> adapter;
+
 
     /**
      * Handle what happens when the view.activity is created
@@ -42,6 +34,7 @@ public class LandingPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.landing_page);
+
         getAdapterWithLanguages();
 
         ListView languageList = findViewById(R.id.languageList);
@@ -49,30 +42,13 @@ public class LandingPage extends AppCompatActivity {
         languageList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                //Toast.makeText(getApplicationContext(), (String) adapterView.getItemAtPosition(i) + " not available",
-                //        Toast.LENGTH_SHORT).show();
-                ((LanguageArrayAdapter)adapter).setLocale(getApplicationContext(), i, LandingPage.this);
-
+                ((LanguageArrayAdapter) adapter).setLocale(getApplicationContext(), i, LandingPage.this);
             }
         });
 
         assignListenerToViews();
         Toolbar myToolbar = findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
-    }
-
-    // TODO: This function can be moved to a separate menu class as its
-
-    /**
-     * Set up menu options
-     *
-     * @param menu: Menu options (https://pasteboard.co/Jc4U58s.png) to be shown in the view.activity
-     * @return: false so that menu option (3 dots) is not shown
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        return false;
     }
 
     /**
@@ -111,6 +87,39 @@ public class LandingPage extends AppCompatActivity {
      */
     void getAdapterWithLanguages() {
         adapter = new LanguageArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, languages);
+                android.R.layout.simple_list_item_1);
+    }
+
+    /**
+     * Set up menu options
+     *
+     * @param menu: Menu options (https://pasteboard.co/Jc4U58s.png) to be shown in the view.activity
+     * @return: true on no error
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_logged_out, menu);
+        return true;
+    }
+
+    /**
+     * Responds to menu option (https://pasteboard.co/Jc4U58s.png) of this view.activity
+     *
+     * @param item: The item in the menu that is selected
+     * @return: return false in case of error, true otherwise
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case (R.id.about_us):
+                startActivity(new Intent(LandingPage.this, AboutUs.class));
+                return true;
+            default:
+                Toast.makeText(getApplicationContext(), "Oops! Error. You shouldn't be seeing this message",
+                        Toast.LENGTH_SHORT).show();
+                // Add code to report bug
+                return false;
+        }
     }
 }
